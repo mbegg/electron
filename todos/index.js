cb7a35008e9e58fb52d0a8f5ba7680f00553b85a
Matt Begg <mattb@simba.com>
2018-01-30 21:51:21 -0800

    add todo example

---
+++ todos/index.js
@@ -0,0 +1,68 @@
+const electron = require('electron');
+
+const {app, BrowserWindow, Menu, ipcMain} = electron;
+
+let mainWindow;
+let addWindow;
+
+app.on('ready', ()=>{
+    mainWindow = new BrowserWindow({});
+    mainWindow.loadURL(`file://${__dirname}/main.html`);
+    mainWindow.on('closed',()=>app.quit());
+    const mainMenu = Menu.buildFromTemplate(menuTemplate);
+    Menu.setApplicationMenu(mainMenu);
+});
+
+function createAddWindow(){
+    addWindow = new BrowserWindow({
+        width: 300,
+        height: 200,
+        title: 'Add New Todo',
+        type: 'modal'
+    });
+    addWindow.loadURL(`file://${__dirname}/add.html`);
+    addWindow.on('closed',()=>addWindow = null);
+}
+
+ipcMain.on('todo:add', (event,todo)=>{
+    mainWindow.webContents.send('todo:add', todo);
+    addWindow.close();
+});
+
+const menuTemplate = [
+    {
+        label: 'File',
+        submenu:[
+            {
+                label:'Add Todo',
+                click(){
+                    createAddWindow();
+                }
+            },
+            {
+                label:'Quit',
+                click(){
+                    app.quit();
+                },
+                accelerator: process.platform === 'darwin' ? 'Command+Q' : 'Ctrl+Q'
+            }]
+}];
+
+if(process.platform === 'darwin'){
+    menuTemplate.unshift({})
+}
+
+if(process.env.NODE_ENV !== 'production'){
+    menuTemplate.push({
+        label: 'Developer',
+        submenu:[
+            {
+                label:"Toggle Developer Tools",
+                accelerator:process.platform === 'darwin' ? 'Command+Alt+I' : "Ctrl+Shift+I",
+                click(item, focusedWindow){
+                    focusedWindow.toggleDevTools();
+                }
+            }
+        ]
+    })
+}
